37301c804b2c51d0998304d42fd1c0aa175cc9cb
lior2911 <aytegev1@gmail.com>
2022-05-22 20:37:22 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,94 @@
+
+// function getRandomNumber(){
+//   var rnd = Math.floor(Math.random()*11)
+//   return rnd
+// }
+// console.log(getRandomNumber())
+
+// function getRandomNumber(num){
+  
+//   var rnd = Math.floor(Math.random()*num)
+//   return rnd
+// }
+// console.log(getRandomNumber(7))
+
+// function getNumberFromUser(someNum){
+//   someNum = +prompt("enter a num")
+//   var rnd = Math.floor(Math.random()*11)
+//   if(someNum==rnd){
+//     console.log("succsess")
+//   }
+//   else{
+//     console.log("failure")
+//   }
+// }
+// getNumberFromUser()
+
+// function getArray(){
+//   var someNum = []
+//   for(var i =0; i<10;i++){
+//     someNum.push(Math.floor(Math.random()*101))
+
+//   }
+//   console.log(someNum)
+// }
+// getArray()
+
+// function someNum(){
+//   var num = +prompt()
+//   var rnd = Math.floor(Math.random()*10)
+//   if(rnd>num){
+//     return rnd
+//   }
+//   else{
+//     return num
+//   }
+// }
+// console.log(someNum())
+
+// function getMin(num){
+//   var num;
+//   var min = 0
+//   for(var i = 0 ; i <= 5; i++){
+//     num = +prompt()
+
+//     min = Math.min(num)
+//   }
+//   console.log(min)
+// }
+// getMin()
+
+// function getRandomNumber(){
+//   var numFromUser = +prompt();
+//   var rnd = Math.floor(Math.random()*101)
+//   if(numFromUser%2==0){
+//     return Math.min(numFromUser,rnd)
+//   }
+//   else{
+//     return Math.max(numFromUser,rnd)
+//   }
+// }
+// console.log(getRandomNumber())
+
+
+// function someArray(){
+//   var array = Math.floor(Math.random()*873)
+//   var rnd = Math.floor(Math.random()*365)
+//   array.length() 
+//   array.push(rnd)
+
+// }
+// someArray()
+
+
+
+
+
+
+
+
+
+
+
+
+
